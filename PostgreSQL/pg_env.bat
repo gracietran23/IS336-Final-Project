f2d3a45768d7ab1731d573b62155d6644a402241
Phuong Anh <tpa05403@gmail.com>
2024-01-20 14:06:57 +0700

    push odoo

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files (x86)\Odoo 8.0-20170901\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files (x86)\Odoo 8.0-20170901\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files (x86)\Odoo 8.0-20170901\PostgreSQL\share\locale
+
+                          
